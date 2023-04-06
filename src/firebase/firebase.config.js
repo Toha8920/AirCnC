0bd60ac45c29888d9c2fd41eae9db0bdf7b730ab
Toha Islam <islamtoha710@gmail.com>
2023-04-06 11:19:55 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWNU5YqoHD2B6FZrLA1ITcMgePwnbgLuI",
+  authDomain: "aircnc-357a2.firebaseapp.com",
+  projectId: "aircnc-357a2",
+  storageBucket: "aircnc-357a2.appspot.com",
+  messagingSenderId: "526032293004",
+  appId: "1:526032293004:web:bde4b27007cb5d3a053fa9",
+  measurementId: "G-P5RY47KLEW"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export default app
